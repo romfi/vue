6b72146d23c540d2a851050688e6f173782e8e75
Romel Gomez <romel.gomez@qusol.com.au>
2020-02-28 14:40:57 +0800

    Adding a todo item.

--- test_ui/src/App.vue
+++ test_ui/src/App.vue
@@ -1,16 +1,22 @@
 <template>
     <div id="app">
+        <Header />
+        <AddTodo v-on:add-todo="addTodo" />
         <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
     </div>
 </template>
 
 <script>
+import Header from "./components/layout/Header"
 import Todos from "./components/Todos"
+import AddTodo from "./components/AddTodo"
 
 export default {
     name: 'App',
     components: {
-        Todos
+        Header,
+        Todos,
+        AddTodo
     },
     data() {
         return {
@@ -36,6 +42,9 @@ export default {
     methods: {
         deleteTodo(id) {
             this.todos = this.todos.filter(todo => todo.id != id);
+        },
+        addTodo(newTodo) {
+            this.todos = [...this.todos, newTodo];
         }
     }
 }
@@ -51,5 +60,18 @@ export default {
     body {
         font-family: Arial, Helvetica, sans-serif;
         line-height: 1.4
+    }
+
+    .btn {
+        display: inline-block;
+        border: none;
+        background: #555;
+        color: #fff;
+        padding: 7px 20px;
+        cursor: pointer;
+    }
+
+    .btn:hover {
+        background: #666;
     }
 </style>
